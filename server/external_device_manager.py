be30908af0e07dcd09082e9fe3e3ac00147407a7
AleksanderRDL <aleksander1231234@gmail.com>
2024-05-24 03:34:13 +0200

    processing type

--- server/external_device_manager.py
+++ server/external_device_manager.py
@@ -1,17 +1,21 @@
+import logging
 import grpc
 import workloads_pb2
 import workloads_pb2_grpc
 
 class ExternalDeviceManager:
+    logging.basicConfig(level=logging.INFO)
+    
     def __init__(self, address):
         self.address = address
         self.channel = grpc.insecure_channel(address)
         self.video_stub = workloads_pb2_grpc.VideoStreamerStub(self.channel)
         self.task_stub = workloads_pb2_grpc.TaskManagerStub(self.channel)
+        self.processing_type = "motion_detection"
 
-    def stream_video(self, task_id, processing_type, ffmpeg_manager):
+    def stream_video(self, task_id, ffmpeg_manager):
         def generate_video_chunks():
-            first_chunk = workloads_pb2.VideoChunk(processing_type=processing_type)
+            first_chunk = workloads_pb2.VideoChunk(processing_type=self.processing_type)
             yield first_chunk
             while True:
                 chunk = ffmpeg_manager.get_stream_output(task_id)
@@ -21,7 +25,7 @@ class ExternalDeviceManager:
 
         response_iterator = self.video_stub.StreamVideo(generate_video_chunks())
         for response in response_iterator:
-            print(f"Response from server: {response.message}")
+            logging.info(f"Response from server: {response.message}")
         return response
 
     def send_task(self, task_id, task_type, payload):
